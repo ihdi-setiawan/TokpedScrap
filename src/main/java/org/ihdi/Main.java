3cf47800a1aa3eb5cf134e623993a359d073e5ec
ihdi <ihdinawan@recharge.id>
2021-10-11 12:19:19 +0700

    get data link, rating, img, store

--- src/main/java/org/ihdi/Main.java
+++ src/main/java/org/ihdi/Main.java
@@ -42,7 +42,46 @@ public class Main {
             driver.findElement(By.xpath("//article/div/div/div/h1/following-sibling::*[2]/button")).click();
         }
         Thread.sleep(1000);
-        
+
+        List<WebElement> elements;
+        boolean notCompletedYet = true;
+        HashMap<String, List<String>> linkMaps = new HashMap<String, List<String>>();
+
+        while(notCompletedYet) {
+            js.executeScript("window.scrollBy(0, 350);");
+            elements = driver.findElements(By.xpath("//div[@data-cy='categoryProduct']/div/div/div"));
+            for (WebElement element : elements) {
+                List<WebElement> ratings = element.findElements(By.xpath(".//a/div[@data-productinfo='true']/div[@data-integrity='true']/i[@aria-label='Rating Star']/following-sibling::*[1]"));
+                String rating = "-";
+                if (!ratings.isEmpty()) {
+                    rating = ratings.get(0).getText();
+                }
+                if (!ratings.isEmpty() && rating == "5.0") {
+                    js.executeScript("arguments[0].remove()", element);
+                    continue;
+                } else {
+                    List<String> productAttr = new ArrayList<>();
+                    productAttr.add(rating);
+                    productAttr.add(element.findElement(By.xpath(".//a/div/img")).getAttribute("src"));
+                    List<WebElement> stores = element.findElements(By.xpath(".//span[@data-testid='linkShopName']"));
+                    productAttr.add(stores.get(0).getText());
+                    linkMaps.put(element.findElement(By.xpath(".//a")).getAttribute("href"), productAttr);
+                    numOfAds++;
+                    if (numOfAds == totalAllAds) {
+                        notCompletedYet = false;
+                        break;
+                    }
+                    js.executeScript("arguments[0].remove()", element);
+                }
+            }
+
+            if (!notCompletedYet) break;
+
+            Thread.sleep(1000);
+            js.executeScript("window.scrollTo(document.body.scrollHeight, 0)");
+            Thread.sleep(1000);
+        }
+
         driver.close();
     }
 }
